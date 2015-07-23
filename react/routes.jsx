b8645ccda6f870977cae34f677ca061b844b985d
Daniel Kesler <danielckesler@gmail.com>
2015-07-23 11:28:09 -0600

    Update routes.jsx

--- react/routes.jsx
+++ react/routes.jsx
@@ -2,17 +2,4 @@ var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
 
-var Main = require('./routes/Main.jsx');
-var Home = require('./routes/home/Home.jsx');
-var Address = require('./routes/address/Address.jsx');
-var Weather = require('./routes/weather/Weather.jsx');
-
-var routes = (
-  <Route handler={Main}>
-    <Route name="home" path="/" handler={Home} />
-    <Route name="address" path="/address/:lat/:lon" handler={Address} />
-    <Route name="weather" path="weather/:lat/:lon" handler={Weather} />
-  </Route>
-);
-
 module.exports = routes;
