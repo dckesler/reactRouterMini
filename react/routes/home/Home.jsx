10ca6602dcc35316d7f370586847bcdc4817b254
Daniel Kesler <danielckesler@gmail.com>
2015-07-23 11:29:08 -0600

    Update Home.jsx

--- react/routes/home/Home.jsx
+++ react/routes/home/Home.jsx
@@ -1,13 +1,1 @@
 var React = require('react');
-
-var Home = React.createClass({
-  render: function(){
-    return (
-      <div>
-        Home
-      </div>
-    );
-  }
-});
-
-module.exports = Home;
