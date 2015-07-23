ee868adab0f57ea89590decd2a7f0ad5a45d1806
Daniel Kesler <danielckesler@gmail.com>
2015-07-23 11:28:23 -0600

    Update Main.jsx

--- react/routes/Main.jsx
+++ react/routes/Main.jsx
@@ -2,14 +2,5 @@ var React = require("react");
 var Router = require("react-router");
 var RouteHandler = Router.RouteHandler;
 
-var Main = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <RouteHandler />
-      </div>
-    );
-  }
-});
 
 module.exports = Main; 
