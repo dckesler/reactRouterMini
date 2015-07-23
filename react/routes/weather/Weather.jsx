b85e75fc6bd65c2b0dacebbc63289eec4211cbe1
Daniel Kesler <danielckesler@gmail.com>
2015-07-23 11:28:39 -0600

    Update Weather.jsx

--- react/routes/weather/Weather.jsx
+++ react/routes/weather/Weather.jsx
@@ -1,12 +1,1 @@
 var React = require("react");
-var Weather = React.createClass({
-  render: function(){
-    return (
-      <div>
-        Weather
-      </div>
-    );
-  }
-});
-
-module.exports = Weather;
